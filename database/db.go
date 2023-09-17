c10ccfabcc9a8a29fa1ddf1674231b89e3d29135
biskitsx <112327677+biskitsx@users.noreply.github.com>
2023-09-17 22:34:10 +0700

    refactor 🍖

--- database/db.go
+++ database/db.go
@@ -1,20 +1,20 @@
 package database
 
 import (
+	"log"
+
 	"github.com/biskitsx/Server-Side-Session/model"
 	"gorm.io/driver/postgres"
 	"gorm.io/gorm"
 )
 
-var Db *gorm.DB
-
-func ConnectPostgres() error {
-	var err error
+func ConnectPostgres() *gorm.DB {
 	dsn := "host=localhost user=postgres password=postgres port=5432 sslmode=disable TimeZone=Asia/Shanghai"
-	Db, err = gorm.Open(postgres.Open(dsn), &gorm.Config{})
+	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
 	if err != nil {
-		return err
+		log.Fatal(err)
+		return db
 	}
-	Db.AutoMigrate(&model.User{})
-	return nil
+	db.AutoMigrate(&model.User{})
+	return db
 }
--- database/session.go
+++ database/session.go
@@ -6,11 +6,10 @@ import (
 	"github.com/gofiber/fiber/v2/middleware/session"
 )
 
-var Store *session.Store
-
-func CreateSessionStore() {
-	Store = session.New(session.Config{
+func CreateSessionStore() *session.Store {
+	store := session.New(session.Config{
 		CookieHTTPOnly: true,
 		Expiration:     time.Minute * 1,
 	})
+	return store
 }
